2deb8cb414592526d6204e90cb12a1e64cdbdff1
joris.sparla@gmail.com <joris.sparla@gmail.com>
2023-03-13 14:39:14 +0100

    routes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,8 @@
 import { Link } from "@remix-run/react";
-
 import { useOptionalUser } from "~/utils";
 
 export default function Index() {
+  console.log("index");
   const user = useOptionalUser();
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
